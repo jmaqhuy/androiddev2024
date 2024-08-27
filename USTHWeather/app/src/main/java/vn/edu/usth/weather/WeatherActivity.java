28bc499c3cafaac6130909c261a06cdb74fa2a44
Quoc Huy <quochuy.ws@gmail.com>
2024-08-27 20:49:32 +0700

    Lapword2 and 3

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,58 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WeatherActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i("CreateApplication", "onCreate() is being executed!");
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_weather);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i("StartApplication", "onStart() is being executed!");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i("ResumeApplication", "onResume() is being executed!");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i("PauseApplication", "onPause() is being executed!");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i("StopApplication", "onStop() is being executed!");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i("DestroyApplication", "onDestroy() is being executed!");
+        super.onDestroy();
+    }
+}
