a4146936e4040d886c2ab9256bb9cbd75b98d84e
Quoc Huy <quochuy.ws@gmail.com>
2024-09-11 14:49:30 +0700

    update practice 6, add toolbar

--- README.md
+++ README.md
@@ -12,5 +12,5 @@ Student Info
 
 * Name: *Nguyen Quoc Huy*
 * ID: *BA12-089*
-* Group ID: *Your Group ID here*
-* Project Name: *Your Project Name here*
+* Group ID: *11*
+* Project Name: *Github Browser*
--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -3,6 +3,8 @@ package vn.edu.usth.weather;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.appcompat.widget.Toolbar;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -18,8 +20,10 @@ public class WeatherActivity extends AppCompatActivity {
         Log.i("CreateApplication", "onCreate() is being executed!");
         super.onCreate(savedInstanceState);
 
-//        ForecastFragment forecastFragment = new ForecastFragment();
-//        getSupportFragmentManager().beginTransaction().add(R.id.main, forecastFragment).commit();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        getSupportActionBar().setTitle("USTH Weather");
 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_weather);
@@ -29,6 +33,7 @@ public class WeatherActivity extends AppCompatActivity {
             return insets;
         });
     }
+
 
     @Override
     protected void onStart() {
